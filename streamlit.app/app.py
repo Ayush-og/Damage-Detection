1f76ae6c18736e6f0025ec7f72e4d1be483583b1
Ayush-og <ayushog82@gmail.com>
2026-03-01 09:26:06 +0530

    Fixed requirements files

--- streamlit.app/app.py
+++ streamlit.app/app.py
@@ -1,14 +1,21 @@
 import streamlit as st
-from model_helper import predict
+import requests
 
 st.title("Vehicle Damage Detection")
 
 uploaded_file = st.file_uploader("Upload the file", type=["jpg", "png"])
 
 if uploaded_file:
-    image_path = "temp_file.jpg"
-    with open(image_path, "wb") as f:
-        f.write(uploaded_file.getbuffer())
-        st.image(uploaded_file, caption="Uploaded File", use_container_width=True)
-        prediction = predict(image_path)
-        st.info(f"Predicted Class: {prediction}")
+    st.image(uploaded_file, caption="Uploaded File", use_container_width=True)
+
+    # Send file to FastAPI
+    response = requests.post(
+        "http://127.0.0.1:9001/predict",
+        files={"file": uploaded_file}
+    )
+
+    if response.status_code == 200:
+        result = response.json()
+        st.info(f"Predicted Class: {result['prediction']}")
+    else:
+        st.error("Error getting prediction from API")
